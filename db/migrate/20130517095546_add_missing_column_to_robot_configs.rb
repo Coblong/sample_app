b95fc393a0c2625406b394a3c21c08d8587d22f5
Paul Cobley <paul.cobley@gmail.com>
2013-06-12 09:37:20 +0100

    remove shitty migration

--- db/migrate/20130517095546_add_missing_column_to_robot_configs.rb
+++
@@ -1,5 +0,0 @@
-class AddMissingColumnToRobotConfigs < ActiveRecord::Migration
-  def change
-    add_column :robot_configs, :most_consecutive_losses_in_money, :decimal, :precision => 2, :scale => 2
-  end
-end
